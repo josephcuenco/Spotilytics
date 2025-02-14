7ab104186aec4cf59849ba09b0d34b43c7102cd0
josephcuenco <josephcuenco@ufl.edu>
2025-02-13 21:12:23 -0500

    log in msg update

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -8,8 +8,9 @@ const Login = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900">
       <div className="bg-gray-800 p-8 rounded-2xl shadow-lg text-center text-white">
-        <h1 className="text-2xl font-semibold mb-4">Welcome to Spotilytics</h1>
-        <p className="text-gray-400 mb-6">Sign in to explore your music insights.</p>
+        <h1 className="text-2xl font-semibold mb-4">Welcome to Spotilytics!</h1>
+        <p className="text-white mb-6">Discover everything from your top artists and songs to the linguistic trends and musical composition of your music library.</p>
+        <p className="text-gray-400 mb-6">Click the button below to login with Spotify.</p>
         <a
           href={AUTH_URL}
           className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-6 rounded-full shadow-md transition-all"
