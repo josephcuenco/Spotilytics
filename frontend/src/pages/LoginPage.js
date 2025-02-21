1aa6a3654a336a3ef8e5f048c7a0d5a11e73b038
josephcuenco <josephcuenco@ufl.edu>
2025-02-21 15:18:31 -0500

    python set up and first user data retrieval

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-const CLIENT_ID = "c868abc7e737484fa8c9d686cfdff89d";
-const REDIRECT_URI = "http://localhost:3000/dashboard"; 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=user-read-private%20user-read-email`;
+// const CLIENT_ID = "c868abc7e737484fa8c9d686cfdff89d";
+// const REDIRECT_URI = "http://localhost:3000/dashboard"; 
+// const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=user-read-private%20user-read-email`;
+
+const loginWithSpotify = () => {
+  window.location.href = "http://localhost:5000/login";
+};
+
+
 
 const Login = () => {
   return (
@@ -11,12 +17,12 @@ const Login = () => {
         <h1 className="text-2xl font-semibold mb-4">Welcome to Spotilytics!</h1>
         <p className="text-white mb-6">Discover everything from your top artists and songs to the linguistic trends and musical composition of your music library.</p>
         <p className="text-gray-400 mb-6">Click the button below to login with Spotify.</p>
-        <a
-          href={AUTH_URL}
+        <button
+          onClick={loginWithSpotify}
           className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-6 rounded-full shadow-md transition-all"
         >
           Login with Spotify
-        </a>
+        </button>
       </div>
     </div>
   );
